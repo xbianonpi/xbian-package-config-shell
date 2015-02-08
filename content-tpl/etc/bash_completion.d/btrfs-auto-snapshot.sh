cfdd9a36a49ab838077e6ea1faa3d707884c5e50
CurlyMoo <curlymoo1@gmail.com>
2015-02-08 23:18:54 +0100

    New versioning

---
+++ content-tpl/etc/bash_completion.d/btrfs-auto-snapshot.sh
@@ -0,0 +1,17 @@
+_btrfs-auto-snapshot()
+{
+        local cur prev opts
+        COMPREPLY=();
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        if [ "${prev}" == "rollback" -o "${prev}" == "destroy" ]; then
+                arguments=$(btrfs-auto-snapshot list 2>&1 | grep -v '/@$\|^WARNING\:');
+                COMPREPLY=($(compgen -W "${arguments}" -- $cur))
+                return 0;
+        elif [ "${COMP_WORDS[COMP_CWORD-2]}" != "btrfs-auto-snapshot" ]; then
+                opts=$(echo "list snapshot rename destroy listvol rollback createvol xbiancopy backuphome");
+                COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+                return 0;
+        fi
+}
+complete -F _btrfs-auto-snapshot btrfs-auto-snapshot
